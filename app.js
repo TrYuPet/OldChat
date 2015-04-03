1d4fd3c813eb545a1fd64d4907f6ec638440ae48
tryupet <yurik851@gmail.com>
2015-04-04 01:50:56 +0300

    добавил шаблоны

--- app.js
+++ app.js
@@ -1,44 +1,41 @@
 var express = require('express');
 var http = require('http');
 var path = require('path');
-var config = require ('config');
-var log = require ("libs/log")(module)
+var config = require('config');
+var log = require('libs/log')(module);
 
 var app = express();
-app.set('port', config.get('port'));
+// почти тоже самое, что и ejs только есть возможности layout, partial, blok
+//engine. т.к файлы с таким расширением ejs, нужно обрабатывать как ejs-locals
+app.engine('ejs', require ('ejs-locals'));
+app.set('views', __dirname + '/template');
+app.set('view engine', 'ejs');
 
-http.createServer(app).listen(app.get('port'), function(){
-  log.info('Express server listening on port ' + config.get('port'));
+app.use(express.favicon()); // /favicon.ico
+
+//смотрит запросы
+if (app.get('env') == 'development') {
+  app.use(express.logger('dev'));
+} else {
+  app.use(express.logger('default'));
+}
+//считывает формы методом post
+app.use(express.bodyParser());  // доступны в req.body....
+
+//парсит куки
+app.use(express.cookieParser()); // req.cookies
+
+//позволяет говорить, какие запросы и как будут обработаны
+app.use(app.router);
+app.get('/', function(req, res, next) {
+  res.render("index", {
+  });
 });
 
-// Middleware
-app.use(function(req, res, next) {
-  if (req.url == '/') {
-    res.end("Hello");
-  } else {
-    next();
-  }
-});
+//если предедущие middleware не обработаны, то упраление передается этому. и если он дирректории
+//паблик, то ищет файл
+app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-  if (req.url == '/forbidden') {
-    next(new Error("wops, denied"));
-  } else {
-    next();
-  }
-});
-
-app.use(function(req, res, next) {
-  if (req.url == '/test') {
-    res.end("Test");
-  } else {
-    next();
-  }
-});
-
-app.use(function(req, res) {
-  res.send(404, "Page Not Found Sorry");
-});
 
 app.use(function(err, req, res, next) {
   // NODE_ENV = 'production'
@@ -48,4 +45,15 @@ app.use(function(err, req, res, next) {
   } else {
     res.send(500);
   }
+});
+/*
+ var routes = require('./routes');
+ var user = require('./routes/user');
+ // all environments
+ app.get('/', routes.index);
+ app.get('/users', user.list);
+ */
+
+http.createServer(app).listen(config.get('port'), function(){
+  log.info('Express server listening on port ' + config.get('port'));
 });
