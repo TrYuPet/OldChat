48dfb927f30582f380cb65491b247929049fc1f3
tryupet <yurik851@gmail.com>
2015-04-04 00:14:48 +0300

    add project

---
+++ app.js
@@ -0,0 +1,50 @@
+//
+var express = require('express');
+var http = require('http');
+var path = require('path');
+
+var app = express();
+app.set('port', 3000);
+
+http.createServer(app).listen(app.get('port'), function(){
+  console.log('Express server listening on port ' + app.get('port'));
+});
+
+// Middleware
+app.use(function(req, res, next) {
+  if (req.url == '/') {
+    res.end("Hello");
+  } else {
+    next();
+  }
+});
+
+app.use(function(req, res, next) {
+  if (req.url == '/forbidden') {
+    next(new Error("wops, denied"));
+  } else {
+    next();
+  }
+});
+
+app.use(function(req, res, next) {
+  if (req.url == '/test') {
+    res.end("Test");
+  } else {
+    next();
+  }
+});
+
+app.use(function(req, res) {
+  res.send(404, "Page Not Found Sorry");
+});
+
+app.use(function(err, req, res, next) {
+  // NODE_ENV = 'production'
+  if (app.get('env') == 'development') {
+    var errorHandler = express.errorHandler();
+    errorHandler(err, req, res, next);
+  } else {
+    res.send(500);
+  }
+});
